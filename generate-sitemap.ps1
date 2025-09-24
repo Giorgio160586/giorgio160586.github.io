264149a008c423c4e8c7dc15d2858ce7436433c7
Giorgio <giorgio160586@gmail.com>
2025-09-24 18:38:07 +0200

    update

--- generate-sitemap.ps1
+++ generate-sitemap.ps1
@@ -1,26 +1,45 @@
-
 # Ottieni tutti i file .html nella directory corrente e sottodirectory
 $htmlFiles = Get-ChildItem -Path . -Recurse -Include *.html
 
-# Inizio contenuto sitemap XML
+# Inizio contenuto sitemap XML con namespace xhtml
 $sitemapContent = @"
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
 
 "@
 
-# Aggiungi ogni file HTML come entry nel sitemap
-foreach ($file in $htmlFiles) {
-    $url = "https://giorgio160586.github.io/" + $file.FullName.Substring($PWD.Path.Length + 1).Replace("\", "/")
-    $lastmod = (Get-Item $file.FullName).LastWriteTime.ToString("yyyy-MM-dd")
-    $sitemapContent += "  <url>
-"
-    $sitemapContent += "    <loc>$url</loc>
-"
-    $sitemapContent += "    <lastmod>$lastmod</lastmod>
-"
-    $sitemapContent += "  </url>
-"
+# Raggruppa i file per nome relativo senza prefisso lingua (es. resume.html)
+$groupedFiles = $htmlFiles | ForEach-Object {
+    $relativePath = $_.FullName.Substring($PWD.Path.Length + 1).Replace("\", "/")
+    $parts = $relativePath.Split("/")
+    if ($parts.Count -ge 2 -and ($parts[0] -eq "en" -or $parts[0] -eq "it")) {
+        [PSCustomObject]@{
+            Lang = $parts[0]
+            File = $_
+            Relative = $relativePath
+            Key = ($parts[1..($parts.Count-1)] -join "/") # es. resume.html
+        }
+    }
+} | Group-Object Key
+
+foreach ($group in $groupedFiles) {
+    foreach ($entry in $group.Group) {
+        $url = "https://giorgio160586.github.io/" + $entry.Relative
+        $lastmod = (Get-Item $entry.File.FullName).LastWriteTime.ToString("yyyy-MM-dd")
+
+        $sitemapContent += "  <url>`n"
+        $sitemapContent += "    <loc>$url</loc>`n"
+        $sitemapContent += "    <lastmod>$lastmod</lastmod>`n"
+
+        # Aggiungi link alternati per tutte le lingue disponibili dello stesso file
+        foreach ($alt in $group.Group) {
+            $altUrl = "https://giorgio160586.github.io/" + $alt.Relative
+            $sitemapContent += "    <xhtml:link rel=`"alternate`" hreflang=`"$($alt.Lang)`" href=`"$altUrl`" />`n"
+        }
+
+        $sitemapContent += "  </url>`n"
+    }
 }
 
 # Chiudi il tag urlset
@@ -29,4 +48,4 @@ $sitemapContent += "</urlset>"
 # Scrivi il contenuto nel file sitemap.xml con codifica UTF-8 senza BOM
 [System.IO.File]::WriteAllText("sitemap.xml", $sitemapContent, (New-Object System.Text.UTF8Encoding($false)))
 
-Write-Output "Il file sitemap.xml è stato generato correttamente."
+Write-Output "Il file sitemap.xml è stato generato correttamente con hreflang."
